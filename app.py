e17da0db29b72f953ba911d0d1779118591b2ab1
maxfioadmin-cmd <maxfioadmin@gmail.com>
2026-05-21 01:43:29 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import requests
 from datetime import datetime
 
 # URL do Web App (Google Apps Script)
-APPS_SCRIPT_URL = "https://script.google.com/a/macros/maxfiocondutoreseletricos.com.br/s/AKfycbw32UzWTSpXHN3QE60a1a9s82FE8sFf7lp9Y7k_upP1eMA7hflm1OR0zGTabSZQAB2kJg/exec"
+APPS_SCRIPT_URL = "https://script.google.com/a/macros/maxfiocondutoreseletricos.com.br/s/AKfycbzc-vzUamtYpJr58ZSTBFQqJfIx-Ie5jXZRiLREKpGh0mHHVGPZ_LnPWAHhLn7xuCGOFQ/exec"
 
 def salvar_sheets(dados: dict):
     """Envia as respostas para o Google Sheets via Apps Script."""
